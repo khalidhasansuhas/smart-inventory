ba7f7edb19a4ab245cccaa7a367df1b976ca0ca5
khalidhasansuhas <khalidhasansuhas@gmail.com>
2023-05-20 21:38:43 +0600

    navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../assets/logo.jpg'
+
+const Navbar = () => {
+    return (
+        <nav
+            className="absolute inset-x-0 top-0 h-16 flex items-center"
+            style={{
+                background: 'linear-gradient(90deg, #2B5876 0.92%, #464776 95.37%)',
+                width: '100vw',
+            }}
+        >
+            <div className="text-white text-xl font-bold ml-6 items-center w-full">
+                <div className='flex justify-center relative mb-3 w-36'>
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        style={{width:'19.34px',height:'18.87'}}
+                        className=''
+                    />
+                </div>
+                <span className='font-thin text-xs absolute bottom-3 pl-1 '>NEURAL SEMICONDUCTOR</span>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
